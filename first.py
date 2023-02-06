85c1d499054707eb6932556cc7d4132555602d7a
bsuraj231 <93305301+bsuraj231@users.noreply.github.com>
2023-02-06 15:06:14 +0100

    Count funtionaly in arrays

--- first.py
+++ first.py
@@ -1,1 +1,20 @@
-print("python is interpreted Language ");
+import array as arr
+
+
+print("python is interpreted Language ")
+
+a = arr.array("i", [1, 2434, 45])
+
+# for i in range(0, 3):
+#     print(a[i], end=" ")
+# arr.array()e
+
+# car = ["volvo", "mercedes", "audi"]
+# whichCar = car
+# lengthOfArray = len(car)
+# # print(lengthOfArray)
+# print("typecode() " + typecode(car))
+# print("typecode() " + whichCar.typecode())
+
+x = arr.array("d", [1, 2, 3, 3, 2, 2, 7])
+print(x.count(2))
