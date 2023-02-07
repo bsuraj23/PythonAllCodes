d93eeb7d927f26da740799ea6b4c4d8dae1acd17
bsuraj231 <93305301+bsuraj231@users.noreply.github.com>
2023-02-07 17:42:35 +0100

    Class1 Handson for Syntax Python

---
+++ Class2/TypeConvertion.py
@@ -0,0 +1,12 @@
+print("ptython do not have lONG like Java ")
+print("Understand + in print and using comma ")
+
+
+a = 90
+b = 90.78787
+c = "a"
+d = "IamABigWord"
+
+print("a value is ", a)
+print("a value is " + str(a))
+print("printing the value after the monitor in place ")
---
+++ class1/first.py
@@ -0,0 +1,11 @@
+import array as arr
+
+
+print("python is interpreted Language ")
+
+a = arr.array("i", [1, 2434, 45])
+
+
+
+x = arr.array("d", [1, 2, 3, 3, 2, 2, 7])
+print(x.count(2))
