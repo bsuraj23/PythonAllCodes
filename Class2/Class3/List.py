6d703fde8b97592db4f5be6b6c94f994290787db
bsuraj231 <93305301+bsuraj231@users.noreply.github.com>
2023-02-08 17:19:16 +0100

    class3 examples

---
+++ Class2/Class3/List.py
@@ -0,0 +1,18 @@
+print("List is same as List in Java")
+print("Insertion order is preserved")
+print("Hetrogenious Object can be aded")
+print("Duplicates can be added")
+print("Growable in Nature")
+print("Values shall be enclosed with sqaure brackets")
+
+list = ["1", "2", 23, 45.45454]
+print(list)
+print(list[0])
+print(list[-1])
+list[1] = 99909999
+
+print(list)
+list.append("newStringValue")
+print(list)
+list = list * 2
+print("printing List after * 2", list)
